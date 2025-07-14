a1f1eb4d27b3c11636f62e68cedc59864b517990
Pedro Felipe <pfsilva190406@gmail.com>
2025-07-13 23:47:02 -0300

    ...

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/', async () => {
-  return'health check'
+  return 'health check'
 })
 
-app.get('/test', async () => {
-  const test = await knex('sqlite_schema').select('*')
+app.get('/transactions', async () => {
+  const transactions = await knex('transactions').where({
+    id: crypto.randomUUID(),
+    title: 'Teste',
+    amount: 100.00,
+  }).select('*').returning('*')
 
-  return test
+  return transactions
 })
 
 app.listen({
