112a8c2249c29eaa4c0679c4479df20f6c8a3b9d
Pedro Felipe <pfsilva190406@gmail.com>
2025-07-14 23:50:58 -0300

    usando somente a validação do zod

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,6 @@ import { knex as setupknex, Knex } from 'knex'
 import 'dotenv/config'
 import { env } from './env'
 
-if (env.DATABASE_URL) {
-  throw new Error('DATABASE_URL não esta definido')
-}
-
 export const config:Knex.Config = {
   client: 'sqlite3',
   connection: {
